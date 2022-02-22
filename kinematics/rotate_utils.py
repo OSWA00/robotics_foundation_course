fa5f2813d9acb693b010e63d57d971aff46c3527
oswaldo <oswahdez00@gmail.com>
2022-02-22 12:03:06 -0600

    fix radians

--- kinematics/rotate_utils.py
+++ kinematics/rotate_utils.py
@@ -2,6 +2,7 @@ from math import sin, cos, radians
 import numpy as np
 
 def rotate_on_x(alpha):
+    alpha = radians(alpha)
     return np.array([
         [1, 0, 0, 0],
         [0, cos(alpha), -sin(alpha), 0],
@@ -10,6 +11,7 @@ def rotate_on_x(alpha):
     ])
 
 def rotate_on_y(phi):
+    phi = radians(phi)
     return np.array([
         [cos(phi), 0, sin(phi), 0],
         [0, 1, 0, 0],
@@ -18,6 +20,7 @@ def rotate_on_y(phi):
     ])
 
 def rotate_on_z(theta):
+    theta = radians(theta)
     return np.array([
         [cos(theta), -sin(theta), 0, 0],
         [sin(theta), cos(theta), 0, 0],
